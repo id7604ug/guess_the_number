5603ff2cf593023e214f795000d4983c90e037d0
Anthony <@home>
2017-01-19 15:36:05 -0600

    Final version - Fixed tracking amount of times played

--- guess_the_number.py
+++ guess_the_number.py
@@ -4,9 +4,9 @@ correct = 'you guessed correctly!'
 too_low = 'too low'
 too_high = 'too high'
 
-def number_times_play(time_play):
-    '''Display the number of games the user has played'''
-    print("You have played {} game/s so far".format(time_play))
+# def number_times_play(time_play):
+#     '''Display the number of games the user has played'''
+#     print("You have played {} game/s so far".format(time_play))
 
 
 def display_banner():
@@ -45,13 +45,9 @@ def get_guess():
         guess = input('Guess the secret number? ')
 
         if guess.isdigit():
-
             break
-
         else:
             print("that is not a valid entry, please try again.")
-
-
     return int(guess)
 
 def check_guess(guess, secret):
@@ -64,47 +60,37 @@ def check_guess(guess, secret):
         return too_high
 
 def main():
-
-
-    while True:
-        number_times_play(guessCount)
-        guess = get_guess()
+    timesPlayed = 0
 
     continueGame = True
 
     while continueGame == True:
-
         display_banner()
+        print('You have played ' + str(timesPlayed) + ' many times')
         guessCount = 0 # Counts the amount of guesses
         (low, high) = configure_range()
         secret = generate_secret(low, high)
 
-        while True:
-
+        while True: # Validation
             guess = get_guess()
-
             result = check_guess(guess, secret)
             guessCount += 1 # Increments each time the user guesses
             print(result)
-
-            if result == correct:
+            if result == correct: # Check if correct guess
+                timesPlayed += 1 # Increment timesPlayed
                 break
 
         print('You took ' + str(guessCount) + ' guesses') # Displays the total amount of guesses
 
         playAgain = input("Do you want play again? ") # ask user to play again.
 
-        if playAgain.lower() == "y":
-
+        if playAgain.lower() == "y": # Play again
             continueGame = True
-
-        else:
-
+        else: # Stop playing
             continueGame = False
             break
 
     print('You took ' + str(guessCount) + ' guesses') # Displays the total amount of guesses
-    number_times_play(guessCount)
 
 
 if __name__ == '__main__':
